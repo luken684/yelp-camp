64d3bc642f17ca2ffb76b6eee30e605090b09354
luken684 <luken684@gmail.com>
2021-05-18 15:55:30 +0100

    Added in some styles

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i <= 50; i++){
         const random122 = Math.floor(Math.random()* 122);
+        const price = Math.floor(Math.random()* 20) + 10;
         const camp = new Campground ({
             location: `${cities[random122].city}, ${cities[random122].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            price 
         })
         await camp.save()
     }
